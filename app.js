fe6e700c98a2e7565042531d0aacabcb82b7fcb9
droooney <d.vor7@mail.ru>
2016-05-12 01:05:55 +0500

    Allow client get custom headers.

--- app.js
+++ app.js
@@ -12,7 +12,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(/.*/, (req, res, next) => {
   res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Some-Header');
+	res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Foo-Header, Bar-Header, Baz-Header'
+  );
+	res.header(
+    'Access-Control-Expose-Headers',
+    'Foo-Header, Bar-Header, Baz-Header'
+  );
 	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, HEAD');
 	res.header('Access-Control-Allow-Credentials', true);
 
