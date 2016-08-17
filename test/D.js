adecfaee8c77ca0e21cfa9323d1a549c40ddd693
droooney <d.vor7@mail.ru>
2016-08-17 21:52:56 +0500

    D: export assert methods directly.

--- test/D.js
+++ test/D.js
@@ -1,196 +1,213 @@
-import * as assert from 'assert';
-import * as methods from './../lib/D';
+import { strictEqual } from 'assert';
+import {
+  isArray, isArrayLike, isBoolean, isDate, isDateLike, isElement, isFinite,
+  isFunction, isInteger, isIntegerLike, isNaN, isNull, isNullOrUndefined, isNumber,
+  isNumberLike, isObject, isPrimitive, isRegExp, isString, isSymbol, isUndefined
+} from './../lib/D';
 
 /* eslint no-new-wrappers: 0 */
 
 describe('it should test D.', () => {
   describe('isArray()', () => {
     it('should return false with arguments argument', () => {
-      assert.strictEqual(methods.isArray((function () {
-        return arguments;
-      })()), false);
+      strictEqual((function () {
+        return isArray(arguments);
+      })(1, 2, 3), false);
     });
     it('should return true with array argument', () => {
-      assert.strictEqual(methods.isArray([]), true);
+      strictEqual(isArray([]), true);
     });
   });
   describe('isArrayLike()', () => {
     it('should return true with arguments argument', () => {
-      assert.strictEqual((function () {
-        return methods.isArrayLike(arguments);
+      strictEqual((function () {
+        return isArrayLike(arguments);
       })(1, 2, 3), true);
     });
     it('should return true with HTMLCollection argument', () => {
-      assert.strictEqual(methods.isArrayLike(document.getElementsByTagName('body')), true);
+      strictEqual(isArrayLike(document.getElementsByTagName('body')), true);
     });
     it('should return true with a string argument ("")', () => {
-      assert.strictEqual(methods.isArrayLike('123'), true);
+      strictEqual(isArrayLike('123'), true);
     });
   });
   describe('isBoolean()', () => {
     it('should return false with no arguments', () => {
-      assert.strictEqual(methods.isBoolean(), false);
+      strictEqual(isBoolean(), false);
     });
     it('should return true with boolean argument', () => {
-      assert.strictEqual(methods.isBoolean(false), true);
-      assert.strictEqual(methods.isBoolean(true), true);
+      strictEqual(isBoolean(false), true);
+      strictEqual(isBoolean(true), true);
     });
   });
   describe('isDate()', () => {
     it('should return false with date alike argument', () => {
-      assert.strictEqual(methods.isDate('1999-12-31T23:59:59.999Z'), false);
-      assert.strictEqual(methods.isDate(946684799999), false);
+      strictEqual(isDate('1999-12-31T23:59:59.999Z'), false);
+      strictEqual(isDate(946684799999), false);
     });
     it('should return true with date argument', () => {
-      assert.strictEqual(methods.isDate(new Date()), true);
+      strictEqual(isDate(new Date()), true);
     });
   });
   describe('isDateLike()', () => {
     it('should return true with date-like arguments', () => {
-      assert.strictEqual(methods.isDateLike('1999-12-31T23:59:59.999Z'), true);
-      assert.strictEqual(methods.isDateLike(946684799999), true);
-      assert.strictEqual(methods.isDateLike(null), true);
+      strictEqual(isDateLike('1999-12-31T23:59:59.999Z'), true);
+      strictEqual(isDateLike(946684799999), true);
+      strictEqual(isDateLike(null), true);
+    });
+  });
+  describe('isDateLike()', () => {
+    it('should return true with Element arguments', () => {
+      strictEqual(isElement(document.createElement('html')), true);
+      strictEqual(isElement(document.createElement('div')), true);
     });
   });
   describe('isFinite()', () => {
     it('should return false with non-number argument', () => {
-      assert.strictEqual(methods.isFinite('1'), false);
-      assert.strictEqual(methods.isFinite({}), false);
-      assert.strictEqual(methods.isFinite(null), false);
+      strictEqual(isFinite('1'), false);
+      strictEqual(isFinite({}), false);
+      strictEqual(isFinite(null), false);
     });
     it('should return false with NaN, Infinity, -Infinity arguments', () => {
-      assert.strictEqual(methods.isFinite(NaN), false);
-      assert.strictEqual(methods.isFinite(Infinity), false);
-      assert.strictEqual(methods.isFinite(-Infinity), false);
+      strictEqual(isFinite(NaN), false);
+      strictEqual(isFinite(Infinity), false);
+      strictEqual(isFinite(-Infinity), false);
     });
     it('should return true with constructed number argument', () => {
-      assert.strictEqual(methods.isFinite(new Number()), true);
+      strictEqual(isFinite(new Number()), true);
     });
     it('should return true with primitive number argument', () => {
-      assert.strictEqual(methods.isFinite(0), true);
+      strictEqual(isFinite(0), true);
     });
   });
   describe('isFunction()', () => {
     it('should return true with function argument', () => {
-      assert.strictEqual(methods.isFunction(() => {}), true);
+      strictEqual(isFunction(() => {}), true);
     });
   });
   describe('isInteger()', () => {
     it('should return false with string argument', () => {
-      assert.strictEqual(methods.isInteger('1'), false);
+      strictEqual(isInteger('1'), false);
     });
     it('should return true with integer argument', () => {
-      assert.strictEqual(methods.isInteger(1), true);
+      strictEqual(isInteger(1), true);
     });
   });
   describe('isIntegerLike()', () => {
     it('should return true with null argument', () => {
-      assert.strictEqual(methods.isIntegerLike(null), true);
+      strictEqual(isIntegerLike(null), true);
     });
     it('should return true with string argument representing integer', () => {
-      assert.strictEqual(methods.isIntegerLike('1'), true);
+      strictEqual(isIntegerLike('1'), true);
     });
   });
   describe('isNaN()', () => {
     it('should return false with no arguments', () => {
-      assert.strictEqual(methods.isNaN(), false);
+      strictEqual(isNaN(), false);
     });
     it('should return false with a "NaN" string argument', () => {
-      assert.strictEqual(methods.isNaN('NaN'), false);
+      strictEqual(isNaN('NaN'), false);
     });
     it('should return true with NaN argument', () => {
-      assert.strictEqual(methods.isNaN(NaN), true);
+      strictEqual(isNaN(NaN), true);
     });
   });
   describe('isNull()', () => {
     it('should return false with no arguments', () => {
-      assert.strictEqual(methods.isNull(), false);
+      strictEqual(isNull(), false);
     });
     it('should return true with null argument', () => {
-      assert.strictEqual(methods.isNull(null), true);
+      strictEqual(isNull(null), true);
     });
   });
   describe('isNullOrUndefined()', () => {
     it('should return true with no arguments', () => {
-      assert.strictEqual(methods.isNullOrUndefined(), true);
+      strictEqual(isNullOrUndefined(), true);
     });
     it('should return true with null argument', () => {
-      assert.strictEqual(methods.isNullOrUndefined(null), true);
+      strictEqual(isNullOrUndefined(null), true);
     });
   });
   describe('isNumber()', () => {
     it('should return false with string argument', () => {
-      assert.strictEqual(methods.isNumber('1'), false);
+      strictEqual(isNumber('1'), false);
     });
     it('should return true with NaN argument', () => {
-      assert.strictEqual(methods.isNumber(NaN), true);
+      strictEqual(isNumber(NaN), true);
     });
     it('should return true with constructed number argument', () => {
-      assert.strictEqual(methods.isNumber(new Number()), true);
+      strictEqual(isNumber(new Number()), true);
     });
     it('should return true with primitive number argument', () => {
-      assert.strictEqual(methods.isNumber(0), true);
+      strictEqual(isNumber(0), true);
     });
   });
   describe('isNumberLike()', () => {
     it('should return true with null argument', () => {
-      assert.strictEqual(methods.isNumberLike(null), true);
+      strictEqual(isNumberLike(null), true);
     });
     it('should return true with string argument representing number', () => {
-      assert.strictEqual(methods.isNumberLike('1'), true);
-      assert.strictEqual(methods.isNumberLike('1e4'), true);
-      assert.strictEqual(methods.isNumberLike('NaN'), true);
-      assert.strictEqual(methods.isNumberLike('-Infinity'), true);
+      strictEqual(isNumberLike('1'), true);
+      strictEqual(isNumberLike('1e4'), true);
+      strictEqual(isNumberLike('NaN'), true);
+      strictEqual(isNumberLike('-Infinity'), true);
     });
   });
   describe('isObject()', () => {
     it('should return false with primitive arguments', () => {
-      assert.strictEqual(methods.isObject(0), false);
-      assert.strictEqual(methods.isObject('0'), false);
-      assert.strictEqual(methods.isObject(false), false);
+      strictEqual(isObject(0), false);
+      strictEqual(isObject('0'), false);
+      strictEqual(isObject(false), false);
     });
     it('should return true with object argument', () => {
-      assert.strictEqual(methods.isObject({}), true);
+      strictEqual(isObject({}), true);
     });
   });
   describe('isPrimitive()', () => {
     it('should return false with non-primitive arguments', () => {
-      assert.strictEqual(methods.isPrimitive(new String()), false);
-      assert.strictEqual(methods.isPrimitive(new Number()), false);
-      assert.strictEqual(methods.isPrimitive(new Boolean()), false);
-      assert.strictEqual(methods.isPrimitive({}), false);
+      strictEqual(isPrimitive(new String()), false);
+      strictEqual(isPrimitive(new Number()), false);
+      strictEqual(isPrimitive(new Boolean()), false);
+      strictEqual(isPrimitive({}), false);
     });
     it('should return true with primitive argument', () => {
-      assert.strictEqual(methods.isPrimitive(undefined), true);
-      assert.strictEqual(methods.isPrimitive(null), true);
-      assert.strictEqual(methods.isPrimitive(0), true);
-      assert.strictEqual(methods.isPrimitive('0'), true);
-      assert.strictEqual(methods.isPrimitive(false), true);
+      strictEqual(isPrimitive(undefined), true);
+      strictEqual(isPrimitive(null), true);
+      strictEqual(isPrimitive(0), true);
+      strictEqual(isPrimitive('0'), true);
+      strictEqual(isPrimitive(false), true);
 
       if (isFunction(global.Symbol)) {
-        assert.strictEqual(methods.isPrimitive(Symbol()), true);
+        strictEqual(isPrimitive(Symbol()), true);
       }
     });
   });
   describe('isRegExp()', () => {
     it('should return true with regexp argument', () => {
-      assert.strictEqual(methods.isRegExp(/1/), true);
+      strictEqual(isRegExp(/1/), true);
     });
   });
   describe('isString()', () => {
     it('should return true with constructed string argument', () => {
-      assert.strictEqual(methods.isString(new String()), true);
+      strictEqual(isString(new String()), true);
     });
     it('should return true with primitive string argument', () => {
-      assert.strictEqual(methods.isString(''), true);
+      strictEqual(isString(''), true);
     });
   });
+  if (isFunction(global.Symbol)) {
+    describe('isSymbol()', () => {
+      it('should return true with Symbol argument', () => {
+        strictEqual(isSymbol(Symbol('1')), true);
+      });
+    });
+  }
   describe('isUndefined()', () => {
     it('should return false with null argument', () => {
-      assert.strictEqual(methods.isUndefined(null), false);
+      strictEqual(isUndefined(null), false);
     });
     it('should return true with no arguments', () => {
-      assert.strictEqual(methods.isUndefined(), true);
+      strictEqual(isUndefined(), true);
     });
   });
 });
