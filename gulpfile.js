8c6195bf496b9a66126c095dda791f6acf01a9e4
droooney <d.vor7@mail.ru>
2016-09-20 12:45:32 +0500

    Gulp: add build:es6, build:es5 and test:node tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ const sourcemaps = require('gulp-sourcemaps');
 const buffer = require('vinyl-buffer');
 const source = require('vinyl-source-stream');
 const watch = require('rollup-watch');
+const babel = require('rollup-plugin-babel');
 const uglify = require('rollup-plugin-uglify');
 
 const createServer = require('./server');
@@ -53,7 +54,7 @@ gulp.task('default', ['server:dev'], () => {
   });
 });
 
-gulp.task('build', ['build:default', 'build:min']);
+gulp.task('build', ['build:default', 'build:min', 'build:es6', 'build:es5']);
 
 gulp.task('jsdoc', ['server:dev', 'jsdoc:compile'], () => (
   gulp.watch(['./lib/**/*.js'], ['jsdoc:compile'])
@@ -61,6 +62,10 @@ gulp.task('jsdoc', ['server:dev', 'jsdoc:compile'], () => (
 
 gulp.task('jsdoc:public', ['server:dev', 'jsdoc:public:compile'], () => (
   gulp.watch(['./lib/**/*.js'], ['jsdoc:public:compile'])
+));
+
+gulp.task('test:node', () => (
+  run('mocha test/node.js --reporter dot').exec()
 ));
 
 modules.forEach((module) => {
@@ -117,6 +122,30 @@ gulp.task('build:min', () => {
     .pipe(buffer())
     .pipe(sourcemaps.init({ loadMaps: true }))
     .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('build:es6', () => {
+  const config = _.cloneDeep(rollupBuildConfig);
+
+  config.entry = './dwayne.js';
+  config.format = 'es';
+  config.sourceMap = false;
+
+  return rollupStream(config)
+    .pipe(source('es6.js'))
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('build:es5', () => {
+  const config = _.cloneDeep(rollupBuildConfig);
+
+  config.entry = './node.js';
+  config.format = 'cjs';
+  config.sourceMap = false;
+
+  return rollupStream(config)
+    .pipe(source('es5.js'))
     .pipe(gulp.dest('./build'));
 });
 
